a0f6cf347ab6d184dcf27449d389dce2aa1bfa29
Dimitrios Khristoforidi <dimitrios.khristoforidi@gmail.com>
2024-11-19 12:53:03 +0600

    Created backend request

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
+import { useState, useEffect } from "react";
 import { ProductCard } from "./components/ProductCard";
 
-const products = [
-  {
-    id: 0,
-    name: "Product 1",
-    category: "Category 1",
-    price: 200,
-    img: "https://www.istore.co.za/media/catalog/product/cache/4ebc814e413626645aa42e369230a31f/i/p/iphone_16_pro_desert_titanium_pdp_image_position_1__gben_2.png",
-  },
-  {
-    id: 1,
-    name: "Product 2",
-    category: "Category 1",
-    price: 500,
-    img: "https://www.istore.co.za/media/catalog/product/cache/4ebc814e413626645aa42e369230a31f/i/p/iphone_16_pro_desert_titanium_pdp_image_position_1__gben_2.png",
-  },
-];
-
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProducts(json))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <div>
+      {loading && <div>Loading...</div>}
       {products.map((product) => (
         <ProductCard data={product} />
       ))}
